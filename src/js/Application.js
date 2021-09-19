5b7d4d5b981ef060a2cd0e6ef9fafa0dfee3ebf7
aleks <aleks.mihaylov69@gmail.com>
2021-09-19 20:10:32 +0300

    add lyrics to the dom

--- src/js/Application.js
+++ src/js/Application.js
@@ -14,11 +14,19 @@ export default class Application extends EventEmitter {
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
+    this._beat = new Beat();
+    this._create = () => {
+      if (count >= lyrics.length) return;
+      const message = document.createElement("div");
+      message.classList.add("message");
+      message.innerText = lyrics[count];
+      count++;
+      document.querySelector(".main").appendChild(message);
+    };
 
-    document.querySelector(".main").appendChild(message);
+    this._beat.on(Beat.events.BIT, () => {
+      this._create();
+    });
 
     this.emit(Application.events.READY);
   }
